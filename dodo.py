840feeb18eef78acc47730c3ffa8ee5bd45f6d5a
Andreas Trawoeger <atrawog@dorgeln.org>
2022-03-09 09:24:45 +0100

    Copy GH-Pages config

--- dodo.py
+++ dodo.py
@@ -1,5 +1,28 @@
+import shutil
+import os
+
 def task_build():
     """build"""
+    def copyfiles (output,copyfiles):
+        if not os.path.exists(output):
+            os.mkdir(output)
+
+        for f in copyfiles: 
+            if os.path.exists(f):
+                shutil.copyfile(f, os.path.join("_output",f))
+ 
     return {
-        'actions': ['jupyter lite build'],
+        'actions': ['jupyter lite build',(copyfiles)],
+        'params':[
+                {
+                    'name':'output',
+                    'short':'o',
+                    'default':'_output'
+                },
+                {
+                    'name':'copyfiles',
+                    'short':'v',
+                    'default':["CNAME",".nojekyll"]
+                }
+            ],
         }
